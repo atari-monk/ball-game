e3f5ad46b1b517e09a706040bb7c3bd32dd1953f
atari-monk <atari.monk1@gmail.com>
2023-08-09 00:51:21 +0200

    upd server

--- server/src/GameServer.ts
+++ server/src/GameServer.ts
@@ -1,32 +1,40 @@
-import { Express } from 'express';
-import cors from 'cors';
-import { Server } from 'http';
-import { inject, injectable } from 'inversify';
-import { Types } from './di-factory/types';
+import { Express } from 'express'
+import cors from 'cors'
+import { Server } from 'http'
+import { inject, injectable } from 'inversify'
+import { Types } from './di-factory/types'
 
 @injectable()
 export class GameServer {
-  private readonly PORT: string | number;
+  private readonly PORT: string | number
 
   constructor(
     @inject(Types.Server) private readonly app: Express,
     @inject(Types.ServerHttp) private readonly server: Server
   ) {
-    this.PORT = process.env.PORT || 3001;
+    this.PORT = process.env.PORT || 3001
   }
 
   public start() {
-    this.configureMiddleware();
-    this.listen();
+    this.configureMiddleware()
+    this.listen()
   }
 
   private configureMiddleware() {
-    this.app.use(cors());
+    this.app.use((req, res, next) => {
+      res.setHeader(
+        'Access-Control-Allow-Origin',
+        'https://atari-monk.github.io'
+      )
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+      next()
+    })
   }
 
   private listen() {
     this.server.listen(this.PORT, () => {
-      console.log(`Server is running on port ${this.PORT}`);
-    });
+      console.log(`Server is running on port ${this.PORT}`)
+    })
   }
 }
