fb8d4a61d0db31a6caf25d14c0ba333285479489
atari-monk <atari.monk1@gmail.com>
2023-08-08 11:57:00 +0200

    deploy server

--- server/src/game-server.ts
+++ server/src/game-server.ts
@@ -11,3 +11,4 @@ import { ServerDIFactory } from './di-factory/ServerDIFactory';
 const factory: IDIFactory<GameServer> = new ServerDIFactory();
 const gameServer: GameServer = factory.create();
 gameServer.start();
+//deploy
