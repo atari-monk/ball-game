6fc37bad47681f257e5f03e3e52e163633d891cf
atari-monk <atari.monk1@gmail.com>
2023-08-09 14:55:47 +0200

    fix webpack

--- game/webpack.config.js
+++ game/webpack.config.js
@@ -1,32 +1,45 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin') // Add this line
+const { SourceMapDevToolPlugin } = require('webpack') // For source maps
 
 module.exports = {
-    entry: "./src/index.ts",
-    mode: "development",
-    devtool: "source-map",
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "atari-monk-ball-game.js",
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new CopyPlugin({
-            patterns: [{ from: "./package.json", to: "./" }],
-        }),
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/, // Add this rule for HTML files
+        use: 'html-loader',
+      },
     ],
-    stats: { errorDetails: false },
-};
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'atari-monk-ball-game.[contenthash].js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: './package.json', to: './' },
+        { from: './config.json', to: './' },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your HTML template
+    }),
+    new SourceMapDevToolPlugin(), // Add this line for source maps
+  ],
+  stats: { errorDetails: false },
+}
